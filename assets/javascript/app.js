cc568e10e9d63f5db787f1f1c450322191a436a1
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-11-02 16:13:48 -0700

    basic game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,15 @@
+// Varables
+// ==================================================================================
 // Setting the time of game play
-var timer = 101;
+var timer = 10;
 var intervalId;
-
 
 // Defining the variables that will be used to show the game score.
 var correctCount = 0;
 var wrongCount = 0;
 var unansweredCount = 0;
-
-// var questionsArray = [
-
-// 	{
-// 	question01: 'What do you call the small image icons used to express emotions or ideas in digital communication?',
-// 	responses01: [Smiley, Emoji, Gif, Icon],
-// 	anwswer01: this.responses01[0]
-// 	},
-
-// ];
+// ==================================================================================
+// END Varables
 
 
 
@@ -24,69 +17,102 @@ $(document).ready(function() {
 
 	console.log('page is ready');
 
+// Begnning game state
 $('#gameContainer').hide();
-$('#resultsContainer').hide();	
+$('#resultsContainer').hide();
 
+
+
+
+// Click Functions:
+// ==================================================================================
 // Start button hides the start page and then shows the game page with questions
 $('#startButton').on("click", function() {
-    	$('#gameContainer').show();
-    	$('#startButtonDiv').hide();
-    	run();
-    	timerStart();
-	});
+	$('#gameContainer').show();
+	$('#startButtonDiv').hide();
+	run();
+	timerStart();
+});
 
-// Functions for running the clock
-	function run() {
-      intervalId = setInterval(timerStart, 1000);
-    }
+// Done button to complete game and take you to the results screen
+$("#doneButton").on("click", function(){
+	timer = 10;
+	stop();
+	gameEnd();
+});
 
-	function timerStart(){
-		timer--;
-
-	      if (timer === 0) {
-	        stop();
-	        alert("Time Up!");
-	        gameEnd();
-		}
-	$('#gameTimer').html('Time Remaining: ' + timer + ' Seconds');
-	}
-
-// If done button is clicked
-	$("#doneButton").on("click", function(){
-			count = 0;
-			stop();
-			gameEnd();
-			});
-
-// Stop the time at 0 to prevent it from counting negative numbers
-	 function stop() {
-      clearInterval(intervalId);
-    }
-
- // Function and ifs for ending the game
- 	function gameEnd() {
-
- 	var q1 = $('input:radio[name="q1"]:checked').val();
-
-	if(q1 == undefined){
-			unansweredCount++;
-		}
-		else if(q1 == "Emoji"){
-			correctCount++;
-		}
-		else{
-			wrongCount++;
-		}
+// Reply button that takes you back to the start screen
+$('#replayButton').on("click", function () {
+	timer = 10;
+	correctCount = 0;
+	wrongCount = 0;
+	unansweredCount = 0;
+	$('#startButtonDiv').show();
 	$('#gameContainer').hide();
-	$('#resultsContainer').show();	
-	
+	$('#resultsContainer').hide();
+})
+// ==================================================================================
+// END Click Functions
 
 
-	$('#correctAnswers').html(correctCount);
-	$('#wrongAnswers').html(wrongCount);
-	$('#unanswered').html(unansweredCount);
 
+
+// Clock Functions
+// ==================================================================================
+
+// Starts the timer and counts down by one seco
+function run() {
+  intervalId = setInterval(timerStart, 1000);
 }
 
+function timerStart(){
+	timer--;
+
+      if (timer === 0) {
+
+        stop();
+        alert("Time Up!");
+        gameEnd();
+	}
+$('#gameTimer').html('Time Remaining: ' + timer + ' Seconds');
+}
+
+// Stop the time at 0 to prevent it from counting negative numbers
+ function stop() {
+  clearInterval(intervalId);
+}
+
+ // ==================================================================================
+ // END Clock Functions
+
+
+
+// If Statments 
+// ==================================================================================
+ // Function and ifs for ending the game
+function gameEnd() {
+
+var q1 = $('input:radio[name="q1"]:checked').val();
+
+if(q1 == undefined){
+		unansweredCount++;
+	}
+	else if(q1 == "Emoji"){
+		correctCount++;
+	}
+	else{
+		wrongCount++;
+	}
+$('#gameContainer').hide();
+$('#resultsContainer').show();	
+
+// Displaying the resutl counts in the html
+$('#correctAnswers').html(correctCount);
+$('#wrongAnswers').html(wrongCount);
+$('#unanswered').html(unansweredCount);
+
+}
+// ==================================================================================
+// END If Statements
 	
 });
